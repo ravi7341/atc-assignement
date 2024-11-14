36e6b36ef16db4647fb9bfcbac701131d9837806
ravi7341 <ravisinghal401@gmail.com>
2024-11-14 15:29:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,164 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.0.0"
-  name    = "k8s-vpc"
-  cidr    = "10.0.0.0/16"
-  azs     = ["us-west-2a", "us-west-2b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+resource "aws_vpc" "k8s_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "k8s-vpc"
+  }
 }
 
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "18.0.0"
-  cluster_name    = "k8s-cluster"
-  cluster_version = "1.24"
-  subnets         = module.vpc.public_subnets
-  vpc_id          = module.vpc.vpc_id
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id            = aws_vpc.k8s_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "k8s-public-subnet-a"
+  }
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id            = aws_vpc.k8s_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "k8s-public-subnet-b"
+  }
+}
+
+resource "aws_internet_gateway" "k8s_igw" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  tags = {
+    Name = "k8s-igw"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.k8s_igw.id
+  }
+
+  tags = {
+    Name = "k8s-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_a_association" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_b_association" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_service_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ec2_container_registry_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+resource "aws_eks_cluster" "k8s_cluster" {
+  name     = "k8s-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.24"
+
+  vpc_config {
+    subnet_ids = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+    aws_iam_role_policy_attachment.eks_service_policy,
+  ]
+}
+
+resource "aws_eks_node_group" "k8s_node_group" {
+  cluster_name    = aws_eks_cluster.k8s_cluster.name
+  node_group_name = "k8s-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+  instance_types  = ["t3.medium"]
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_ec2_container_registry_policy,
+  ]
+}
+output "cluster_name" {
+  value = aws_eks_cluster.k8s_cluster.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.k8s_cluster.endpoint
+}
+
+output "cluster_certificate_authority" {
+  value = aws_eks_cluster.k8s_cluster.certificate_authority[0].data
+}
+
+output "node_group_name" {
+  value = aws_eks_node_group.k8s_node_group.node_group_name
 }
